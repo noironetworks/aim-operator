d6cc548706467321e302b6bda481298440bc434c
sbagalur <sbagalur@cisco.com>
2025-02-20 14:34:06 -0800

    Adding new fields for the CR

--- api/v1alpha1/ciscoaciaim_types.go
+++ api/v1alpha1/ciscoaciaim_types.go
@@ -17,25 +17,102 @@ limitations under the License.
 package v1alpha1
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+        condition "github.com/openstack-k8s-operators/lib-common/modules/common/condition"
+        "github.com/openstack-k8s-operators/lib-common/modules/storage"
+        metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+        //topologyv1 "github.com/openstack-k8s-operators/infra-operator/apis/topology/v1beta1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+const (
+    // ContainerImage - default fall-back container image for CiscoAciAim if associated env var not provided
+    ContainerImage = "quay.io/podified-antelope-centos9/openstack-ciscoaci-aim:current-podified"
+)
+
+type CiscoAciAimSpecCore struct {
+        // +kubebuilder:validation:Optional
+        // NodeSelector to target subset of worker nodes running this service
+        NodeSelector *map[string]string `json:"nodeSelector,omitempty"`
+
+        // +kubebuilder:validation:Optional
+        // ConfigOverwrite - interface to overwrite default config files like e.g. logging.conf or policy.json.
+        // But can also be used to add additional files. Those get added to the service config dir in /etc/<service> .
+        DefaultConfigOverwrite map[string]string `json:"defaultConfigOverwrite,omitempty"`
+
+        // +kubebuilder:validation:Optional
+        // +kubebuilder:default=1
+        // +kubebuilder:validation:Maximum=32
+        // +kubebuilder:validation:Minimum=0
+        // Replicas of horizon API to run
+        Replicas *int32 `json:"replicas"`
+
+        // ExtraMounts containing conf files
+        // +kubebuilder:default={}
+        ExtraMounts []CiscoAciAimExtraVolMounts `json:"extraMounts,omitempty"`
+
+        // +kubebuilder:validation:Optional
+        // CustomServiceConfig - customize the service config using this parameter to change service defaults,
+        // or overwrite rendered information using raw OpenStack config format. The content gets added to
+        // to /etc/<service>/<service>.conf.d directory as custom.conf file.
+        CustomServiceConfig string `json:"customServiceConfig,omitempty"`
+
+        // +kubebuilder:validation:Optional
+        // +kubebuilder:default=neutron
+        // ServiceUser - optional username used for this service to register in neutron
+        ServiceUser string `json:"serviceUser"`
+
+        // +kubebuilder:validation:Required
+        // MariaDB instance name
+        // Right now required by the maridb-operator to get the credentials from the instance to create the DB
+        // Might not be required in future
+        DatabaseInstance string `json:"databaseInstance"`
+
+        // +kubebuilder:validation:Optional
+        // +kubebuilder:default=neutron
+        // DatabaseAccount - optional MariaDBAccount CR name used for neutron DB, defaults to neutron
+        DatabaseAccount string `json:"databaseAccount"`
+
+        // +kubebuilder:validation:Required
+        // +kubebuilder:default=rabbitmq
+        // RabbitMQ instance name
+        // Needed to request a transportURL that is created and used in Neutron
+        RabbitMqClusterName string `json:"rabbitMqClusterName"`
+
+        /*
+        // +kubebuilder:validation:Optional
+        // TopologyRef to apply the Topology defined by the associated CR referenced
+        // by name
+        TopologyRef *topologyv1.TopoRef `json:"topologyRef,omitempty"`
+        */
+}
 
 // CiscoAciAimSpec defines the desired state of CiscoAciAim
 type CiscoAciAimSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of CiscoAciAim. Edit ciscoaciaim_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+        // +kubebuilder:validation:Required
+        // Cisco Aci Aim Container Image URL
+        ContainerImage string `json:"containerImage"`
+        CiscoAciAimSpecCore `json:",inline"`
 }
 
 // CiscoAciAimStatus defines the observed state of CiscoAciAim
 type CiscoAciAimStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+
+   // Map of hashes to track e.g. job status
+   Hash map[string]string `json:"hash,omitempty"`
+
+   // Conditions
+   Conditions condition.Conditions `json:"conditions,omitempty" optional:"true"`
+
+   // Neutron Database Hostname
+   DatabaseHostname string `json:"databaseHostname,omitempty"`
+
+   // TransportURLSecret - Secret containing RabbitMQ transportURL
+   TransportURLSecret string `json:"transportURLSecret,omitempty"`
+
+   // ObservedGeneration - the most recent generation observed for this
+   // service. If the observed generation is less than the spec generation,
+   // then the controller has not processed the latest changes injected by
+   // the opentack-operator in the top-level CR (e.g. the ContainerImage)
+   ObservedGeneration int64 `json:"observedGeneration,omitempty"`
 }
 
 // +kubebuilder:object:root=true
@@ -61,4 +138,32 @@ type CiscoAciAimList struct {
 
 func init() {
 	SchemeBuilder.Register(&CiscoAciAim{}, &CiscoAciAimList{})
+}
+
+func SetupDefaults() {
+    ciscoAciAimDefaults := CiscoAciAimDefaults{
+        ContainerImageURL: util.GetEnvVar("RELATED_IMAGE_CISCOACI_AIM_IMAGE_URL_DEFAULT", ContainerImage),
+    }
+
+}
+
+// CiscoAciAimExtraVolMounts exposes additional parameters processed by the horizon-operator
+// and defines the common VolMounts structure provided by the main storage module
+type CiscoAciAimExtraVolMounts struct {
+	// +kubebuilder:validation:Optional
+	Name string `json:"name,omitempty"`
+	// +kubebuilder:validation:Optional
+	Region string `json:"region,omitempty"`
+	// +kubebuilder:validation:Required
+	VolMounts []storage.VolMounts `json:"extraVol"`
+}
+
+// Propagate is a function used to filter VolMounts according to the specified
+// PropagationType array
+func (c *CiscoAciAimExtraVolMounts) Propagate(svc []storage.PropagationType) []storage.VolMounts {
+	var vl []storage.VolMounts
+	for _, gv := range c.VolMounts {
+		vl = append(vl, gv.Propagate(svc)...)
+	}
+	return vl
 }
